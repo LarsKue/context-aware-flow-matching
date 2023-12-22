2bad4c98758fb95a9ae9dafb620652e93885cd66
LarsKue <lars@kuehmichel.de>
2023-12-22 14:55:24 +0100

    Add Sampling Callback

--- src/models/model.py
+++ src/models/model.py
@@ -1,16 +1,17 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-from torch import Tensor
+from torch import Size, Tensor
 from torch.utils.data import DataLoader
 
 from lightning_trainable import Trainable, TrainableHParams
+from lightning_trainable.hparams import Range
 
 import src.losses as L
 import src.utils as U
 
+from src.callbacks import SampleCallback
 from src.dataloaders import FlowMatchingDataLoader
 
 from . import pools
@@ -84,13 +85,7 @@ class Model(Trainable):
         mmd = L.mmd_loss(c, torch.randn_like(c), reduction="none")
         mmd_mean = mmd.mean(0)
 
-        c = c.unsqueeze(1)
-        c = U.expand_dim(c, 1, set_size)
-        t = U.unsqueeze_right(t, xt.dim() - 1)
-        t = U.expand_dim(t, 1, set_size)
-
-        xtc = torch.cat([xt, t, c], dim=2)
-        v = self.flow(xtc)
+        v = self.velocity(xt, t, c)
 
         mse = U.mean_except(torch.square(v - vstar), 0)
         mse_mean = mse.mean(0)
@@ -103,6 +98,34 @@ class Model(Trainable):
             mse=mse_mean,
             mmd=mmd_mean,
         )
+
+    def velocity(self, x: Tensor, t: Tensor | float, c: Tensor) -> Tensor:
+        if isinstance(t, float):
+            t = torch.full((x.shape[0],), fill_value=t, device=self.device)
+
+        t = U.unsqueeze_right(t, x.dim() - t.dim())
+        t = U.expand_dim(t, 1, x.shape[1])
+
+        c = c.unsqueeze(1)
+        c = U.expand_dim(c, 1, x.shape[1])
+
+        xtc = torch.cat([x, t, c], dim=2)
+
+        return self.flow(xtc)
+
+    def sample(self, sample_shape: Size = (1,), steps: int = 100) -> Tensor:
+        c = torch.randn(sample_shape[0], self.hparams.embeddings, device=self.device)
+        x = torch.randn(sample_shape[0], sample_shape[1], self.hparams.features, device=self.device)
+
+        t = 0.0
+        dt = 1.0 / steps
+
+        for i in range(steps):
+            v = self.velocity(x, t, c)
+            x = x + v * dt
+            t = t + dt
+
+        return x
 
     def _convert_dataloader(self, dataloader: DataLoader) -> DataLoader:
         # we don't want to rewrite all the customizing logic, so just cast to our custom class
@@ -120,3 +143,8 @@ class Model(Trainable):
 
     def test_dataloader(self) -> DataLoader | list[DataLoader]:
         return self._convert_dataloader(super().test_dataloader())
+
+    def configure_callbacks(self):
+        callbacks = super().configure_callbacks()
+        callbacks.append(SampleCallback())
+        return callbacks
