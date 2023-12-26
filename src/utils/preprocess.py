24221d665a7db4c65b1966defc93353887df0b42
LarsKue <lars@kuehmichel.de>
2023-12-26 06:21:54 +0100

    add interpolate

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -2,6 +2,8 @@
 import torch
 from torch import Tensor
 
+from .shapes import unsqueeze_right
+
 
 def normalize(x: Tensor, dim: int) -> Tensor:
     """
@@ -11,3 +13,18 @@ def normalize(x: Tensor, dim: int) -> Tensor:
     std = torch.std(x, dim=dim, keepdim=True)
 
     return (x - mean) / std
+
+
+def interpolate(x: Tensor, y: Tensor, steps: int) -> Tensor:
+    """
+    Linearly interpolate between x and y
+
+    @param x: Tensor of shape (n, ...)
+    @param y: Tensor of shape (n, ...)
+    @param steps: Number of interpolation steps
+
+    @return: Tensor of shape (n, steps, ...)
+    """
+    t = torch.linspace(0.0, 1.0, steps, device=x.device)
+    t = unsqueeze_right(t, x.dim() - 1)
+    return t[None, :] * y[:, None] + (1 - t[None, :]) * x[:, None]
